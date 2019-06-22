57c961c4d73ba1643f3569ba417e45c93599b061
norbertkadar <norbert.kadar.a@gmail.com>
2019-06-22 11:03:00 +0300

    Add project files.

---
+++ Lab3Movie/Controllers/CommentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3Movie.Services;
+using Lab3Movie.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3Movie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private ICommentService commentService;
+
+        public CommentsController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+
+        /// <summary>
+        /// Get all comments.
+        /// </summary>
+        /// <param name="filterString">Optional, filter by text</param>
+        /// <param name="page">Page</param>
+        /// <returns>List of comments</returns>
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        // GET: api/Comments
+        [HttpGet]
+        public PaginatedList<CommentGetModel> Get([FromQuery]string filterString, [FromQuery]int page = 1)
+        {
+            page = Math.Max(page, 1);
+            return commentService.GetAll(page, filterString);
+        }
+    }
+}
---
+++ Lab3Movie/Services/CommentService.cs
@@ -0,0 +1,43 @@
+﻿using Lab3Movie.Models;
+using Lab3Movie.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3Movie.Services
+{
+    public interface ICommentService
+    {
+        PaginatedList<CommentGetModel> GetAll(int page, string filterString);
+    }
+    public class CommentService : ICommentService
+    {
+        private MoviesDbContext context;
+        public CommentService(MoviesDbContext context)
+        {
+            this.context = context;
+        }
+
+        public PaginatedList<CommentGetModel> GetAll(int page, string filterString)
+        {
+            IQueryable<Comment> result = context
+                .Comments
+                .Where(c => string.IsNullOrEmpty(filterString) || c.Text.Contains(filterString))
+                .OrderBy(c => c.Id)
+                .Include(c => c.Movie);
+            var paginatedResult = new PaginatedList<CommentGetModel>();
+            paginatedResult.CurrentPage = page;
+
+            paginatedResult.NumberOfPages = (result.Count() - 1) / PaginatedList<CommentGetModel>.EntriesPerPage + 1;
+            result = result
+                .Skip((page - 1) * PaginatedList<CommentGetModel>.EntriesPerPage)
+                .Take(PaginatedList<CommentGetModel>.EntriesPerPage);
+            paginatedResult.Entries = result.Select(c => CommentGetModel.FromComment(c)).ToList();
+
+            return paginatedResult;
+        }
+
+    }
+}
